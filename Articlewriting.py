9c215b05196e9a1c47842b639120950712fc23fd
mjayant276 <mjayant.276@gmail.com>
2024-02-04 17:01:25 +0530

    Add files via upload

---
+++ Articlewriting.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import streamlit as st
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+_ = load_dotenv(find_dotenv())
+openai_key = os.getenv('OPENAI_API_KEY')
+clt = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_key
+    )
+
+def main():
+    st.title("Article Writer")
+    notes = st.text_area("Enter Topic Information:")
+    content = "I want you to write short literature review on topic "+notes
+    if st.button("Generate Article"):
+        with st.spinner("Generating Article..."):
+            response = clt.chat.completions.create(
+              model="gpt-3.5-turbo",
+              messages=[{'role':'user','content':content}]
+            )
+        description = response.choices[0].message.content
+        st.subheader("Generated Writeup:")
+        st.write(description)
+
+
+if __name__ == '__main__':
+   main()
+
